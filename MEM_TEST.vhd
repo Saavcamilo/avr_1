0ccf84a5351d0f07a71820933c6f694221c10199
Anant Desai <Anant@Anants-MacBook-Air.local>
2017-04-07 22:02:48 -0700

    testing stuff

--- MEM_TEST.vhd
+++ MEM_TEST.vhd
@@ -45,6 +45,10 @@ use ieee.numeric_std.all;
 library opcodes;
 use opcodes.opcodes.all;
 
+--include this library for file handling in VHDL.
+library std;
+use std.textio.all;  --include package textio.vhd
+
 
 entity  MEM_TEST  is
 
@@ -154,6 +158,16 @@ end  Component;
     signal WriteXYZ  :  std_logic;
 
 
+    --period of clock,bit for indicating end of file.
+    signal clock,endoffile : bit := '0';
+    --data read from the file.
+    signal    dataread : real;
+    --data to be saved into the output file.
+    signal    datatosave : real;
+    --line number of the file read or written.
+    signal    linenumber : integer:=1; 
+
+
 begin
 
     -- Unit Under Test port map
@@ -182,5 +196,40 @@ begin
         RegXYZSel => RegisterXYZSel, InputXYZ => InputXYZ, WriteXYZ => WriteXYZ,
         RegAOut => ResultA, RegBOut => ResultB, RegXYZOut => ResultXYZ
     );
+
+    CLK: process
+    begin
+        clock <= '1';
+        wait for 5 ns; -- define a clock
+        clock <= '0';
+        wait for 5 ns;
+    end process CLK;
+              
+
+    reading :
+    process
+        file   infile    : text is in  "memInput.txt";   --declare input file
+        variable  inline    : line; --line number declaration
+        variable  dataread1    : real;
+    begin
+    wait until clock = '1' and clock'event;
+    if (not endfile(infile)) then   --checking the "END OF FILE" is not reached.
+    readline(infile, inline);       --reading a line from the file.
+      --reading the data from the line and putting it in a real type variable.
+    read(inline, dataread1);
+    dataread <=dataread1;   --put the value available in variable in a signal.
+    else
+    endoffile <='1';         --set signal to tell end of file read file is reached.
+    end if;
+
+end process reading;
+
+    -- now generate the stimulus and test it
+    process
+    begin  -- of stimulus process
+
+    
+
+
 
 end  architecture;
